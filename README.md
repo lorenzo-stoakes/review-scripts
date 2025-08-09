f439feebb1a54e388756286efe6f531987530bf5
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2025-08-09 16:04:52 +0100

    initial code commit

---
+++ README.md
@@ -0,0 +1,197 @@
+# review-scripts
+
+A series of scripts to help reviewing of kernel patch series. These are
+essentially light wrappers around other tooling to make review faster.
+
+DISCLAIMER: I don't guarantee these won't lose you work, destroy your git repos,
+            cause lego to fall beneath your bare foot such that you step on it
+            or any other such calamities and I disclaim all responsibility for
+            such! Use these at your own risk.
+
+Note this will generate ugly local tags to store metadata of the form
+review-xxx. Using `review-stop` or `review-clean` will clean these up.
+
+It will also create branches of the form `review/[name]-[version]`. Use
+`review-clean` to get rid of these.
+
+## Dependencies
+
+* usual coreutils etc.
+* git (obviously)
+* b4
+* git-delta
+* virtme-ng
+* neomutt
+* ansi2txt
+* delta
+
+## Installation
+
+1. Ensure dependencies are installed.
+2. Put this directory on your `$PATH`.
+
+It is recommended that you run:
+
+```
+git config --global am.threeWay true
+```
+
+So that you can resolve conflicts in series when they are applied.
+
+### Hooks
+
+Kernel configuration and build scripts are located in the `hooks/` subdirectory:
+
+* `kernel-config` - run once before build.
+* `kernel-build` - run for each build.
+
+## Scripts
+
+**IMPORTANT**: b4 isn't always great at finding all the versions of a
+series. Therefore you may need to have separate review names for separate
+revisions. You can use `review-diff-pair` to workaround this issue for range
+diff comparisons.
+
+* `review-start [name] [head] [msgid]` - Start a review which you're naming
+  `name`, which you're basing on revision `head` and where `msgid` is the msgid
+  that exists in lore for a message in one of the revisions you intend to
+  review. Runs `review-get` on startup to setup review branches.
+
+* `review-get [name]` - Retrieves latest messages for all revisions and updates
+  local branches, placing them in `review/name-vN` braanches.
+
+* `review-stop [name]` - Remove tag for review but leave branches around.
+
+* `review-clear [name]` - Clear everything (tag and branches) for the specified
+  review.
+
+* `review-clear-branches [name]` - Just clear the branches, leave the review
+  active.
+
+* `review-diff [name]` - Provide a side-by-side range-diff between the latest
+  revision in the series and the prior one.
+
+* `review-diff-pair [previous name] [current name] <previous version> <current
+  version>` - Same as review-diff, except it's comparing ranges across separate
+  reviews. Useful for series that b4 loses track of, or RFC -> non-RFC, etc.
+
+* `review-check [name] <version>` - Build each patch. Defaults to the latest
+  version, unless specified. Afterwards this runs `checkpatch.pl` against each
+  patch.
+
+* `review-check [name] <version>` - Just run `checkpatch.pl` against all patches
+  in series.
+
+* `review-read [name]` - Retrieve the mail for all versions of the series and
+  all replies, and load it in neomutt.
+
+## Credit
+
+Thanks to [Tomáš Janoušek](https://genserver.social/users/liskin) for his
+incredible
+[article](https://work.lisk.in/2023/10/19/side-by-side-git-range-diff.html) on
+implementing the side-by-side diff for git range-diff.
+
+I include his `git-range-diff-delta-preproc` helper script under the MIT license.
+
+## Example
+
+## Start review, fixup conflict, grab again
+
+```
+# Start review, grab series
+]$ review-start procmap_query mm-unstable 20250804231552.1217132-1-surenb@google.com
+--- retrieving data for [procmap_query]... ---
+Grabbing thread from lore.kernel.org/all/20250804231552.1217132-1-surenb@google.com/t.mbox.gz
+...
+Applying: selftests/proc: test PROCMAP_QUERY ioctl while vma is concurrently modified
+Applying: fs/proc/task_mmu: factor out proc_maps_private fields used by PROCMAP_QUERY
+Using index info to reconstruct a base tree...
+M	fs/proc/internal.h
+M	fs/proc/task_mmu.c
+Falling back to patching base and 3-way merge...
+Auto-merging fs/proc/internal.h
+Auto-merging fs/proc/task_mmu.c
+CONFLICT (content): Merge conflict in fs/proc/task_mmu.c
+Patch failed at 0002 fs/proc/task_mmu: factor out proc_maps_private fields used by PROCMAP_QUERY
+error: Failed to merge in the changes.
+hint: Use 'git am --show-current-patch=diff' to see the failed patch
+hint: When you have resolved this problem, run "git am --continue".
+hint: If you prefer to skip this patch, run "git am --skip" instead.
+hint: To restore the original branch and stop patching, run "git am --abort".
+hint: Disable this message with "git config set advice.mergeConflict false"
+
+WARNING: shazam failed, if due to conflict, please resolve and re-run command.
+$ # fix conflict
+$ git am --continue
+$ review-get
+...
+Unable to find revision 1
+b4 cannot find version 1, deleting branch 'review/procmap_query-v1'
+Deleted branch review/procmap_query-v1 (was c617a4dd7102).
+$ git branch | grep review
+  review/procmap_query-v2
+  review/procmap_query-v3
+* review/procmap_query-v4
+```
+
+Now all versions of the series, apart from the one b4 can't find (v1) are in
+branches.
+
+```
+$ review-diff procmap_query
+```
+
+![screenshot of side-by-side ranged diff](screenshot_diff.png)
+
+Now I can check that all patches in the latest revision pass checkpatch.pl:
+
+```
+$ review-checkpatch procmap_query
+--- retrieving data for [procmap_query]... ---
+Grabbing thread from lore.kernel.org/all/20250804231552.1217132-1-surenb@google.com/t.mbox.gz
+Checking for newer revisions
+Grabbing search results from lore.kernel.org
+  Added from v3: 4 patches
+  Added from v4: 4 patches
+23 messages in the thread
+Saved /tmp/tmp.DNbDXaXVd5/review-procmap_query.mbx
+--- ...retrieving data for [procmap_query] ---
+Analyzing 23 messages in the thread
+Looking for additional code-review trailers on lore.kernel.org
+Analyzing 9 code-review messages
+Checking attestation on all messages, may take a moment...
+---
+  [PATCH v4 1/3] selftests/proc: test PROCMAP_QUERY ioctl while vma is concurrently modified
+    ● checkpatch.pl: passed all checks
+  [PATCH v4 2/3] fs/proc/task_mmu: factor out proc_maps_private fields used by PROCMAP_QUERY
+    ● checkpatch.pl: passed all checks
+  [PATCH v4 3/3] fs/proc/task_mmu: execute PROCMAP_QUERY ioctl under per-vma locks
+    + Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
+    ● checkpatch.pl: passed all checks
+  ---
+  NOTE: install dkimpy for DKIM signature verification
+---
+Total patches: 3
+---
+Cover: ./v4_20250808_surenb_execute_procmap_query_ioctl_under_per_vma_lock.cover
+ Link: https://lore.kernel.org/r/20250808152850.2580887-1-surenb@google.com
+ Base: using specified base-commit c2144e09b922d422346a44d72b674bf61dbd84c0
+       git checkout -b v4_20250808_surenb_google_com c2144e09b922d422346a44d72b674bf61dbd84c0
+       git am ./v4_20250808_surenb_execute_procmap_query_ioctl_under_per_vma_lock.mbx
+```
+
+Build test every commit in latest revision:
+
+```
+$ review-check procmap_query
+<kernel build automatically performed for every commit in series>
+```
+
+Read all mails in the thread:
+
+```
+$ review-read procmap_query
+```
+
+![screenshot of reading mail direct](screenshot_read.png)
---
+++ __review-shared.sh
@@ -0,0 +1,535 @@
+#!/bin/bash
+set -e; set -o pipefail
+
+# Courtesty of https://stackoverflow.com/a/246128
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+function say()
+{
+	echo "$@" >&2
+}
+
+function fatal()
+{
+	say "$@"
+	exit 1
+}
+
+function error()
+{
+	fatal "ERROR: $@."
+}
+
+function warn()
+{
+	say "WARNING: $@."
+}
+
+function usage()
+{
+	fatal "usage: $(basename $0) $@"
+}
+
+function rev_exists()
+{
+	git rev-parse --verify -q $1 >/dev/null
+}
+
+function tree_clean()
+{
+	git diff-index --quiet HEAD --
+}
+
+function get_curr_ref()
+{
+	git rev-parse --abbrev-ref HEAD
+}
+
+function get_ref_hash()
+{
+	git rev-parse $1
+}
+
+function show_rev_summary()
+{
+	git --no-pager log --oneline -n1
+}
+
+# Retrieve the contents of the tag's message.
+#
+# Params:
+#	$1 - tag name.
+function get_tag_msg()
+{
+	local tag=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "get_tag_msg() requires tag parameter"
+	fi
+
+	git tag -ln $tag | awk '{print $2}'
+}
+
+# Pushes directory onto pushd stack without outputting anything.
+#
+# Params:
+# 	$1 - directory to add to pushd stack.
+function push()
+{
+	pushd $1 >/dev/null
+}
+
+# Pops directory off pushd stack without outputting anything.
+function pop()
+{
+	popd &>/dev/null || true
+}
+
+# Is this a kernel directory?
+function is_kernel_dir()
+{
+	[[ -e MAINTAINERS ]]
+}
+
+# Traverse to root of kernel directory tree. pop will restore previous
+# directory.
+function push_kernel_root_dir()
+{
+	push .
+
+	while [[ $PWD != "/" ]] && ! is_kernel_dir; do
+		cd ..
+	done
+
+	if [[ $PWD == "/" ]]; then
+		error "not in a kernel tree"
+	fi
+}
+
+# Check if environment is sane for review commands in general, and enter the
+# root of the kernel directory if we are indeed in one.
+function check_prep()
+{
+	push_kernel_root_dir
+
+	if ! tree_clean; then
+		error "changes in tree, aborting"
+	fi
+}
+
+# Create a temporary directory, and set a trap so it is always cleaned up.
+# Outputs the temporary directory name.
+function get_temp_dir()
+{
+	local tmpdir="$(mktemp -d)"
+
+	# Just in case, as we are about to rm -f $tmpdir/*
+	if [[ -z "$tmpdir" ]]; then
+		error "empty temporary directory??"
+	fi
+
+	echo "$tmpdir"
+}
+
+# Clean up the directory on exit. Assumes no subdirectories, so we can delete
+# safely.
+#
+# Params:
+#	$1 - name of directory to clean up on exit.
+function cleanup_dir_on_exit()
+{
+	local dir=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "cleanup_dir_on_exit() requires dir parameter"
+	fi
+
+	if [[ -z "$dir" ]]; then
+		error "empty directory specified"
+	fi
+
+	if ! [[ -d "$dir" ]]; then
+		error "directory '$dir' does not exist"
+	fi
+
+	# Ensure we clean up after ourselves.
+	# We avoid rm -rf in case for safety.
+	trap "rm -f $dir/*; rmdir $dir" EXIT
+}
+
+# Get the tag name used to figure out the base commit for this review.
+#
+# Params:
+#	$1 - the name given to the review.
+function get_review_tag()
+{
+	local name=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "get_review_base_tagname() requires name parameter"
+	fi
+
+	echo "review-$name-base"
+}
+
+# Retrieve the msgid we stored for the review.
+#
+# Params:
+#	$1 - the name given to the review.
+function get_review_msgid()
+{
+	local name=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "get_review_msgid() requires name parameter"
+	fi
+
+	local tag="$(get_review_tag $name)"
+	get_tag_msg $tag
+}
+
+# Determine the name we assign to the downloaded mbox based on review name.
+#
+# Params:
+#	$1 - the name given to this review.
+function get_mbox_filename()
+{
+	local name=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "get_mbox_filename() requires name parameter"
+	fi
+
+	echo "review-$name.mbx"
+}
+
+# Retrieve the mbox for the specified msgid including all revisions and store in
+# the specified temporary directory.
+#
+# Params:
+#	$1 - the name given to this review.
+#	$2 - msgid to retrieve.
+#	$3 - tmpdir to put it in.
+#	$4 - try to be vaguely quiet (optional).
+#
+# Outputs path to mbox on exit.
+function retrieve_mbox()
+{
+	local name="$1"
+	local msgid=$2
+	local tmpdir=$3
+	local quiet=$4
+
+	if [[ $# -lt 3 ]]; then
+		error "get_mbox() requires name, msgid, tmpdir parameters"
+	fi
+
+	local mbox_filename="$(get_mbox_filename $name)"
+
+	# It's noisy anyway, so add prefix/suffix.
+	if [[ -z "$quiet" ]]; then
+		echo "--- retrieving data for [$name]... ---" >&2
+		b4 mbox -c $msgid -o $tmpdir -n ${mbox_filename}
+	else
+		b4 -q mbox -c  $msgid -o $tmpdir -n ${mbox_filename}
+	fi
+
+	if [[ -z "$quiet" ]]; then
+		echo "--- ...retrieving data for [$name] ---" >&2
+	fi
+
+	echo "$tmpdir/${mbox_filename}"
+}
+
+function __get_latest_revision()
+{
+	local mbox_path=$1
+
+	# The below won't retrieve v1, so we must wrap this operation.
+
+	# A bit horrible, but does the job...
+	grep -Ei "\[.*PATCH.*\]|\[.*RFC.*\]" "${mbox_path}" | \
+		grep -Eo 'v[0-9]+' | \
+		sed -nE 's/v([0-9]+)/\1/p' | \
+		sort -nr | \
+		head -n1
+}
+
+# Figure out the latest revision number from an mbox of all revisions.
+#
+# Params:
+#	$1 - path to the mbox file.
+#
+# Outputs the latest revision as an integer.
+function get_latest_revision()
+{
+	local mbox_path=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "get_latest_revision() requires mbox path"
+	fi
+
+	revision="$(__get_latest_revision ${mbox_path})"
+	if [[ -z "$revision" ]]; then
+		# OK this could be v1.
+		if grep -Eqi "\[PATCH.+\]|\[PATCH RFC.+\]|\[RFC PATCH.+\]|\[RFC.+\]" "${mbox_path}"; then
+			revision=1
+		else
+			error "Cannot determine revision in '${mbox_path}'"
+		fi
+	fi
+
+	echo $revision
+}
+
+# Get the branch name used for review.
+#
+# Params:
+# 	$1 - the name given to the review.
+#	$2 - the version number.
+function get_review_branch()
+{
+	local name=$1
+	local ver=$2
+
+	if [[ $# -lt 2 ]]; then
+		error "get_review_branch() requires name, version parameters"
+	fi
+
+	echo "review/$name-v$ver"
+}
+
+# b4 shazam the specified version of the series into the current branch.
+#
+# Params:
+#	$1 - the revision the user was previously on.
+#	$2 - the path to the mbox containing the series-es.
+#	$3 - the version to shazam.
+function shazam_at_version()
+{
+	local prev_rev=$1
+	local mbox_path=$2
+	local ver=$3
+
+	if [[ $# -lt 2 ]]; then
+		error "shazam_at_version() requires mbox path, version parameters"
+	fi
+
+	# Bizarrely, b4 will happily fail to get the version you ask for
+	# and... not return an error code. Let's pipe its output into a
+	# temporary file so we can figure out if this happens.
+	tmpfile=$(mktemp)
+
+	# Highly recommended to set am.threeWay for this.
+
+	if ! b4 shazam -m "${mbox_path}" -v $ver 2>&1 | tee $tmpfile >&2; then
+		echo >&2 # Extra line to separate out from noise.
+		warn "shazam failed, if due to conflict, please resolve and re-run command"
+		rm $tmpfile
+		exit 1
+	fi
+
+	# Anyway this at least means we can ignore these cases.
+	if grep -q "Unable to find revision" $tmpfile; then
+		branch=$(get_curr_ref)
+		say "b4 cannot find version $ver, deleting branch '$branch'"
+		git checkout -q $prev_rev
+		git branch -D $branch
+	fi
+
+	rm $tmpfile
+}
+
+# Retrieve all review branches belong to a given review name.
+#
+# Params:
+#	$1 - the name given to the review.
+function get_review_branches()
+{
+	local name=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "get_review_branches() requires name parameter"
+	fi
+
+	git for-each-ref --format='%(refname:short)' refs/heads/ | \
+		grep -E "^review/$name-v[0-9]+$"
+}
+
+# Clear down review branches for specified name.
+#
+# Params:
+#	$1 - the name given to the review.
+#	$2 - ref to return to should we find ourselves on a review branch
+function clear_review_branches()
+{
+	local name=$1
+	local return_branch=$2
+
+	if [[ $# -lt 1 ]]; then
+		error "clear_review_branches() requires name parameter"
+	fi
+
+	local branches="$(get_review_branches $name)"
+
+	# If we're on one of the review branches, need to move off.
+	if [[ $branches =~ "$(get_curr_ref)" ]]; then
+		if rev_exists $return_branch; then
+			git checkout -q $return_branch
+		fi
+	fi
+
+	if [[ -n "$branches" ]]; then
+		git branch -Df $branches
+	else
+		true
+	fi
+}
+
+# Determine if the review has started or not.
+#
+# Params:
+#	$1 - the name given to the review.
+function has_review_started()
+{
+	local name=$1
+
+	if [[ $# -lt 1 ]]; then
+		error "has_review_started() requires name parameter"
+	fi
+
+	tag="$(get_review_tag $name)"
+
+	rev_exists $tag
+}
+
+# Extract an mbox from a larger mbox at a specific version.
+#
+# Params:
+# 	$1 - larger mbox path.
+#	$2 - directory to place files in.
+#	$3 - filename for extracted mbox.
+#	$4 - version to extract.
+#
+# Outputs the path of the mbox file.
+function extract_version_mbox()
+{
+	local mbox_path=$1
+	local dir=$2
+	local filename=$3
+	local version=$4
+
+	b4 -q am -m "${mbox_path}" -o "$dir" -n "${filename}" -v $version
+
+	echo "$dir/$filename"
+}
+
+# Extract the git range-diff command b4 diff _would_ use if -n weren't
+# specified, then outputs its parameters.
+#
+# Params:
+#	$1 - the path of the mbox containing the previous version.
+#	$2 - the path of the mbox containing the current version.
+#
+# Outputs git range-diff parameters.
+function get_range_diff_params()
+{
+	local prev_path=$1
+	local curr_path=$2
+
+	if [[ $# -lt 2 ]]; then
+		error "get_range_diff_params() requires prev_path, curr_path parameters"
+	fi
+
+	# b4 outputs this to stderr so have to fiddle around here. We use tee as
+	# errors might occur too and we want them displayed.
+	tmpfile=$(mktemp)
+	b4 diff -n -m "${prev_path}" "${curr_path}" 2>&1 | tee $tmpfile >&2
+
+	# Retrieve the final line containing the command and trim whitespace.
+	tail -n1 $tmpfile | awk '{$1=$1};1' | sed 's/git range-diff //'
+	rm $tmpfile
+}
+
+# Used to make the git range-diff command 'fancy' - customise if needed.
+function __fancy_range_diff()
+{
+	# We lose line numbers unfortunately.
+	git -c pager.range-diff='
+        git-range-diff-delta-preproc \
+        | delta \
+                --side-by-side \
+                --file-style=omit \
+                --line-numbers-left-format="" \
+                --line-numbers-right-format="│ " \
+                --hunk-header-style=syntax \
+        ' range-diff $@
+}
+
+# Perform a fancy looking side-by-side ranged diff of the
+#
+# Params:
+#	$1 - the path of the mbox containing the previous version.
+#	$2 - the path of the mbox containing the current version.
+function fancy_range_diff()
+{
+	local prev_path=$1
+	local curr_path=$2
+
+	if [[ $# -lt 2 ]]; then
+		error "fancy_range_diff() requires prev_path, curr_path parameters"
+	fi
+
+	# We have to hack a bit here - we can't tell b4 to use a specific pager,
+	# so instead get it to tell us the git range-diff command, which we
+	# extract then adjust to do what we want.
+	params=$(get_range_diff_params "${prev_path}" "${curr_path}")
+	__fancy_range_diff $params
+}
+
+# Perform a ranged diff across two separate larger mboxes at different versions.
+# A larger mbox can contain multiple versions.
+#
+# Params:
+#	$1 - the name given to the previous series review.
+#	$2 - the name given to the current series review.
+#	$3 - the path of the mbox containing the previous series to compare.
+#	$4 - the path of the mbox containing the current series to compare.
+#	$5 - the temporary directory in which to store files.
+#	$6 - the version of the previous series to compare.
+#	$7 - the version of the current series to compare.
+function review_range_diff()
+{
+	local prev_name=$1
+	local curr_name=$2
+	local prev_mbox_path=$3
+	local curr_mbox_path=$4
+	local tmpdir=$5
+	local prev_version=$6
+	local curr_version=$7
+
+	if [[ $# -lt 5 ]]; then
+		error "range_diff() requires prev_name, prev_mbox_path, curr_mbox_path, tmpdir, prev_version, curr_version parameters"
+	fi
+
+	local prev_filename="review-${prev_name}-v${prev_version}.mbx"
+	local curr_filename="review-${curr_name}-v${curr_version}.mbx"
+
+	# Extract versions.
+	local prev_path=$(extract_version_mbox "${prev_mbox_path}" "$tmpdir" "${prev_filename}" "${prev_version}")
+	local curr_path=$(extract_version_mbox "${curr_mbox_path}" "$tmpdir" "${curr_filename}" "${curr_version}")
+
+	fancy_range_diff "${prev_path}" "${curr_path}"
+}
+
+function checkpatch_range()
+{
+	local mbox_path=$1
+	local version=$2
+
+	if [[ $# -lt 2 ]]; then
+		error "checkpatch_range requires mbox_path, version parameters"
+	fi
+
+	b4 am -k -m "${mbox_path}" -v ${version}
+}
